a72f455d7405de748239a1b4c5c3bb114c4531a5
= <=>
2019-10-31 07:27:46 +0900

    Create product page and pagination

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,12 +1,50 @@
 var db = require('../db');
 
 module.exports.index = function(req, res) {
-	var page = req.query.page || 1; //n
-	var perPage = 8; //x
+	var page = parseInt(req.query.page) || 1; //n
+	var limit = parseInt(req.query.limit) || 8; //x
+	var start = (page - 1) * limit;
+	var end = page * limit;
 
-	var start = (page - 1) * perPage;
-	var end = page * perPage;
+	var results = {};
+	var products = db.get('products').value();
+
+	results.pages = [];
+
+	for (var i = 1; i <= Math.ceil(products.length / limit); i++) {
+		results.pages.push({
+			page: i,
+			limit: limit
+		});
+	}
+
+	if (end < products.length) {
+		results.next = {
+			page: page + 1,
+			limit: limit
+		};
+	} else {
+		results.next = {
+			page: page,
+			limit: limit
+		};
+	}
+
+	if (start > 0) {
+		results.previous = {
+			page: page - 1,
+			limit: limit
+		};
+	} else {
+		results.previous = {
+			page: 1,
+			limit: limit
+		};
+	}
+
+	results.products = products.slice(start, end);	
+
 	res.render('products/index', {
-		products: db.get('products').value().slice(start, end)
+		results
 	});
 };
